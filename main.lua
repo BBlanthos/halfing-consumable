ea09ea2f7eb95034bf51a6635086cdf49cfaa647
BBlanthos <159204109+BBlanthos@users.noreply.github.com>
2026-02-02 22:01:24 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,25 @@
+SMODS.Consumable
+	set = "Tarot",
+	key = "halfing",
+	loc_txt = {
+		name = 'halfing consumable',
+		text = { 'use on a joker to half values'}
+},
+	pos = { x = 10, y = 2 },
+	cost = 0,
+
+in_pool = function() return false end,
+
+	can_use = function(self, card)
+		local cards = Cryptid.get_highlighted_cards({ G.jokers }, card, 1, 1, function(card)
+			return not Card.no(card, "immutable", true)
+		end)
+		return #cards == 1
+	end,
+	use = function(self, card, area, copier)
+		local cards = Cryptid.get_highlighted_cards({ G.jokers }, card, 1, 1, function(card)
+			return not Card.no(card, "immutable", true)
+		end)
+		Cryptid.manipulate(cards[1], { value = 0.5 })
+	end,
+	}
